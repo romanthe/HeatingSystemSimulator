3a2c21c9b3cb132e408e29331b284fee6ba00c11
Michal Wiktorowski <m.a.wiktorowski@gmail.com>
2012-11-08 23:04:44 +0100

    Add client interface, send register request periodically.

---
+++ B1/connection/client.go
@@ -0,0 +1,49 @@
+// Connection handler package.
+package connection
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+// Configure client and start infinite time trigered requests
+// TODO Add port and rest parameters
+func Client(sem chan bool) {
+	for {
+		DialDLS(sem, UsePort)
+		<-time.After(HelloTime * time.Second)
+	}
+
+}
+
+
+//TODO change channel semaphore to be more efficient
+func DialDLS(sem chan bool, port string) {
+	
+	sem <- true
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", DLSIp + ":" + UsePort)
+	checkError(err)
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	checkError(err)
+	
+	var buf [512]byte
+	
+	s := registerRequest(port)
+	fmt.Println("Send request:", s)
+	
+	_, err1 := conn.Write([]byte(s))
+	if err1 != nil {
+		return
+	}
+	
+	n, err2 := conn.Read(buf[0:])
+	if err2 != nil {
+		return
+	}
+	<- sem
+	
+	fmt.Println("Recieve response: ", string(buf[0:n]))
+	processRegisterResponse(string(buf[0:n]))
+}
---
+++ B1/connection/request.go
@@ -0,0 +1,22 @@
+package connection
+
+import (
+	mp "sandbox/messageparser"
+)
+
+func registerRequest(port string) string {
+	req := mp.Message{
+		"id":     []string{MyID},
+		"action": []string{"registerRequest"},
+		"port":     []string{port},
+	}
+	return req.Encode()
+}
+
+func processRegisterResponse(s string) int {
+	//resp := mp.ParseMessage(s)// need but only if i have to process info
+	//unpack needed Value and call instruction
+	// func foobar()
+	//fmt.Println(resp["status"])// only for debug version
+	return 0// only for debug version
+}
